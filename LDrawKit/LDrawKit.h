f8027ec258571ac3cbaa41725f8e7454228bfad7
Shane Whitehead <obishiro@gmail.com>
2018-01-20 15:58:21 +1100

    Initial Commit

---
+++ LDrawKit/LDrawKit.h
@@ -0,0 +1,19 @@
+//
+//  LDrawKit.h
+//  LDrawKit
+//
+//  Created by Shane Whitehead on 20/1/18.
+//  Copyright © 2018 Shane Whitehead. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for LDrawKit.
+FOUNDATION_EXPORT double LDrawKitVersionNumber;
+
+//! Project version string for LDrawKit.
+FOUNDATION_EXPORT const unsigned char LDrawKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LDrawKit/PublicHeader.h>
+
+
